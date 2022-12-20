db598ad9be8b5fedc8d11c42772806cfb9b3d388
sw5227 <96852939+sw5227@users.noreply.github.com>
2022-12-19 16:08:51 -0800

    Adan performance analysis uploaded

---
+++ Resnet18 with Adan, Adam and SGD/mainADAM.py
@@ -0,0 +1,220 @@
+'''Train CIFAR10 with PyTorch.'''
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+
+
+from adan import Adan
+import torchvision
+import torchvision.transforms as transforms
+
+import os
+import argparse
+
+from models import *
+from utils import progress_bar
+
+from time import perf_counter
+
+data_loading_time = []
+training_time = []
+total_time = []
+
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--resume', '-r', action='store_true',
+                    help='resume from checkpoint')
+args = parser.parse_args()
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+best_acc = 0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
+# Data
+print('==> Preparing data..')
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+trainset = torchvision.datasets.CIFAR10(
+    root='./data', train=True, download=True, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(
+    trainset, batch_size=128, shuffle=True, num_workers=8)
+
+testset = torchvision.datasets.CIFAR10(
+    root='./data', train=False, download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(
+    testset, batch_size=100, shuffle=False, num_workers=8)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck')
+
+# Model
+print('==> Building model..')
+# net = VGG('VGG19')
+net = ResNet18()
+# net = PreActResNet18()
+# net = GoogLeNet()
+# net = DenseNet121()
+# net = ResNeXt29_2x64d()
+# net = MobileNet()
+# net = MobileNetV2()
+# net = DPN92()
+# net = ShuffleNetG2()
+# net = SENet18()
+# net = ShuffleNetV2(1)
+# net = EfficientNetB0()
+# net = RegNetX_200MF()
+# net = SimpleDLA()
+net = net.to(device)
+if device == 'cuda':
+    net = torch.nn.DataParallel(net)
+    cudnn.benchmark = True
+
+
+
+
+if args.resume:
+    # Load checkpoint.
+    print('==> Resuming from checkpoint..')
+    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+    checkpoint = torch.load('./checkpoint/ckpt.pth')
+    net.load_state_dict(checkpoint['net'])
+    best_acc = checkpoint['acc']
+    start_epoch = checkpoint['epoch']
+
+criterion = nn.CrossEntropyLoss()
+# optimizer = optim.SGD(net.parameters(), lr=args.lr, 
+#                       momentum=0.9, weight_decay=5e-4)
+# optimizer = optim.Adagrad(net.parameters(), lr=args.lr, weight_decay=5e-4)
+# optimizer = optim.Adadelta(net.parameters(), lr=args.lr, weight_decay=5e-4)
+# optimizer = optim.Adam(net.parameters(), lr=args.lr, weight_decay=5e-4)
+optimizer = optim.Adam(net.parameters())
+
+# optimizer = Adan(net.parameters(), lr=args.lr, weight_decay=5e-4)
+
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+
+
+train_acc_list = []
+test_acc_list = []
+train_loss_list = []
+test_loss_list = []
+
+# Training
+def train(epoch):
+    print('\nEpoch: %d' % epoch)
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        
+        t1_start = perf_counter()
+        inputs, targets = inputs.to(device), targets.to(device)
+        t1_end = perf_counter()
+
+        data_loading_time.append(t1_end - t1_start)
+        
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                     % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+    train_loss_list.append(train_loss/(batch_idx+1))
+    train_acc_list.append(100.*correct/total)
+        
+
+
+def test(epoch):
+    global best_acc
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                         % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+    test_loss_list.append(test_loss/(batch_idx+1))
+    test_acc_list.append(100.*correct/total)
+
+    # Save checkpoint.
+    acc = 100.*correct/total
+    if acc > best_acc:
+        print('Saving..')
+        state = {
+            'net': net.state_dict(),
+            'acc': acc,
+            'epoch': epoch,
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        torch.save(state, './checkpoint/ckpt_Adam.pth')
+        best_acc = acc
+
+
+for epoch in range(100):
+
+    t3_start = perf_counter()
+    
+    t2_start = perf_counter()
+    train(epoch)
+    t2_end = perf_counter()
+    
+    training_time.append(t2_end - t2_start)
+    
+    
+    test(epoch)
+    scheduler.step()
+    
+    t3_end = perf_counter()
+    total_time.append(t3_end - t3_start)
+
+print("train loss: ---------------------------")
+print(train_loss_list)
+print("train acc: ---------------------------")
+print(train_acc_list)
+
+print("test loss: ---------------------------")
+print(test_loss_list)
+print("test acc: ---------------------------")
+print(test_acc_list)
+
+# for i in range(5):
+#     print("in epoch ", i, " :" )
+#     print("data loading time: ", data_loading_time[i])
+#     print("training time: ", training_time[i])
+#     print("total time: ", total_time[i])
+    
+# # total number of parameters
+# pytorch_total_params = sum(p.numel() for p in net.parameters())
+# print('total number of parameters = ', pytorch_total_params)
